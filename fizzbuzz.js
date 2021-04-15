ef44764015bba8d73724d02d43b67ede9daebbda
Danilo  Prieto <danilo.s.prieto@gmail.com>
2021-04-15 09:15:10 -0300

    created functions and test

---
+++ fizzbuzz.js
@@ -0,0 +1,40 @@
+function fizzbuzz(num) {
+
+    if (typeof(num) !== 'number') {
+
+        return 'Error: It is not a number';
+    }
+
+    if (num === 0){
+
+        return 0;
+
+    }
+    if (num % 5 === 0 && num % 3 === 0) {
+
+        return 'fizzbuzz';
+
+    }
+    if (num % 3 === 0) {
+        
+        return 'fizz';
+    
+    }
+    if (num % 5 === 0) {
+        
+        return 'buzz';
+
+    }
+    
+    return num;
+}
+
+function print(num) {
+    for(let i=0; i <= num; i++){
+        console.log(`${i}: ${fizzbuzz(i)}`);
+    }
+}
+
+print(16);
+
+module.exports = fizzbuzz;
